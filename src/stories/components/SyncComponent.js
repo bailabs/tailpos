eb9472cdc38835400066ea2e47e7de6ac2014277
Ivan Ray Altomera <irayspacii@gmail.com>
2019-04-11 12:11:30 +0800

    update sync component

--- src/stories/components/SyncComponent.js
+++ src/stories/components/SyncComponent.js
@@ -1,128 +1,162 @@
 import * as React from "react";
-import { Dimensions, View } from "react-native";
+import { Dimensions, View, StyleSheet, TouchableOpacity } from "react-native";
 import { Text, Button, Card, CardItem, Input } from "native-base";
 import { Col, Grid } from "react-native-easy-grid";
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 
-class CompanyComponent extends React.Component {
-  constructor(props) {
-    super(props);
+class CompanyComponent extends React.PureComponent {
+  onSync = () => this.props.sync("sync")
+  onForceSync = () => this.props.sync("forceSync")
+  onSyncEdit = () => this.props.onSyncEdit(true)
+
+  renderSyncButtons() {
+    const {
+      url,
+      user_name,
+      password
+    } = this.props;
+
+    let component = null;
+
+    if (url && user_name && password) {
+      component = (
+        <View style={styles.view}>
+          <Button onPress={this.onSync}>
+            <Text>Sync</Text>
+          </Button>
+          <Button
+            style={styles.lastButton}
+            onPress={this.onForceSync}
+          >
+            <Text>Force Sync</Text>
+          </Button>
+        </View>
+      );
+    }
+
+    return component;
   }
 
   render() {
+    const {
+      url,
+      user_name,
+      password,
+      syncEditStatus,
+      onSyncSave,
+      changeUrl,
+      changeUserName,
+      changePassword,
+    } = this.props;
+
     return (
       <View>
-        <Card
-          style={{
-            width: Dimensions.get("window").width * 0.7,
-            alignSelf: "center",
-          }}
-        >
-          <CardItem
-            style={{
-              backgroundColor: "#4B4C9D",
-              width: Dimensions.get("window").width * 0.7,
-            }}
-          >
+        <Card style={styles.card}>
+          <CardItem style={styles.cardItem}>
             <Grid>
-              <Col
-                style={{
-                  alignSelf: "center",
-                  width: Dimensions.get("window").width * 0.7 * 0.83,
-                }}
-              >
-                <Text
-                  style={{
-                    fontSize: Dimensions.get("window").width * 0.02,
-                    color: "white",
-                  }}
-                >
+              <Col style={styles.col}>
+                <Text style={styles.textHeader}>
                   Sync Settings
                 </Text>
               </Col>
-              <Col
-                style={{
-                  alignSelf: "center",
-                  width: Dimensions.get("window").width * 0.05,
-                }}
-              >
-                <Icon
-                  name="pencil"
-                  size={30}
-                  style={{ color: "white" }}
-                  onPress={() => this.props.onSyncEdit(true)}
-                />
-              </Col>
-              <Col
-                style={{
-                  alignSelf: "center",
-                  width: Dimensions.get("window").width * 0.05,
-                }}
-              >
-                <Icon
-                  name="content-save"
-                  size={30}
-                  style={{ color: "white" }}
-                  onPress={() => this.props.onSyncSave()}
-                />
+              <Col>
+                <View style={styles.viewRight}>
+                  <TouchableOpacity onPress={this.onSyncEdit}>
+                    <Icon
+                      name="pencil"
+                      size={30}
+                      style={styles.icon}
+                    />
+                  </TouchableOpacity>
+                  <TouchableOpacity>
+                    <Icon
+                      name="content-save"
+                      size={30}
+                      style={styles.icon}
+                      onPress={onSyncSave}
+                    />
+                  </TouchableOpacity>
+                </View>
               </Col>
             </Grid>
           </CardItem>
           <CardItem>
             <Input
-              disabled={!this.props.syncEditStatus}
               style={{
-                borderColor: this.props.syncEditStatus ? "#ca94ff" : "#cfcfcf",
+                borderColor: syncEditStatus ? "#ca94ff" : "#cfcfcf",
                 borderWidth: 1,
               }}
-              value={this.props.url}
-              onChangeText={text => this.props.changeUrl(text)}
+              disabled={!syncEditStatus}
+              value={url}
+              onChangeText={changeUrl}
               placeholder="https://erpnext.com"
             />
           </CardItem>
           <CardItem>
             <Input
-              disabled={!this.props.syncEditStatus}
               style={{
-                borderColor: this.props.syncEditStatus ? "#ca94ff" : "#cfcfcf",
+                borderColor: syncEditStatus ? "#ca94ff" : "#cfcfcf",
                 borderWidth: 1,
               }}
-              value={this.props.user_name}
-              onChangeText={text => this.props.changeUserName(text)}
+              disabled={!syncEditStatus}
+              value={user_name}
+              onChangeText={changeUserName}
               placeholder="Administrator"
             />
           </CardItem>
           <CardItem>
             <Input
-              disabled={!this.props.syncEditStatus}
               style={{
-                borderColor: this.props.syncEditStatus ? "#ca94ff" : "#cfcfcf",
+                borderColor: syncEditStatus ? "#ca94ff" : "#cfcfcf",
                 borderWidth: 1,
               }}
-              value={this.props.password}
-              onChangeText={text => this.props.changePassword(text)}
+              value={password}
+              secureTextEntry={true}
+              disabled={!syncEditStatus}
+              onChangeText={changePassword}
               placeholder="Password"
             />
           </CardItem>
           <CardItem>
-            {this.props.url && this.props.user_name && this.props.password ? (
-              <View style={{ flexDirection: "row" }}>
-                <Button onPress={() => this.props.sync("sync")}>
-                  <Text>Sync</Text>
-                </Button>
-                <Button
-                  style={{ marginLeft: 10 }}
-                  onPress={() => this.props.sync("forceSync")}
-                >
-                  <Text>Force Sync</Text>
-                </Button>
-              </View>
-            ) : null}
+            {this.renderSyncButtons()}
           </CardItem>
         </Card>
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  card: {
+    alignSelf: "center",
+    width: Dimensions.get("window").width * 0.7,
+  },
+  cardItem: {
+    marginBottom: 15,
+    backgroundColor: "#4b4c9d",
+    width: Dimensions.get("window").width * 0.7,
+  },
+  col: {
+    alignSelf: "center",
+  },
+  textHeader: {
+    color: "white",
+    fontSize: Dimensions.get("window").width * 0.02,
+  },
+  icon: {
+    color: "white",
+    marginLeft: 10,
+  },
+  view: {
+    flexDirection: "row",
+  },
+  lastButton: {
+    marginLeft: 10,
+  },
+  viewRight: {
+    flexDirection: "row",
+    alignSelf: "flex-end",
+  }
+});
 
 export default CompanyComponent;
