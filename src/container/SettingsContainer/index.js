903ef3d58bef454cb13049e2c6c05f87cd0a324a
jiloysss <Jiloygwapo123>
2019-07-02 15:03:41 +0800

    fix: Company can now be saved with tax refactor: None feat: None

--- src/container/SettingsContainer/index.js
+++ src/container/SettingsContainer/index.js
@@ -337,7 +337,6 @@ export default class SettingsContainer extends React.Component {
       this.props.stateStore.settings_state[0].tax,
     );
     if (this.props.printerStore.companySettings.length > 0) {
-
       let company = this.props.printerStore.findCompany(
         this.props.printerStore.companySettings[0]._id,
       );
