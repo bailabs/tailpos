1ab3ec57d2cbad3b06d66bd8015c0d94d6b3f31f
jiloysss <Jiloygwapo123>
2019-07-22 11:33:55 +0800

    fix: None refactor: None feat: Automatic Sync via Background Job (Execute in 30 sec)

--- src/container/ListingContainer/index.js
+++ src/container/ListingContainer/index.js
@@ -18,6 +18,8 @@ import Listing from "@screens/Listing";
 import InputCategory from "@screens/InputCategory";
 import InputDiscount from "@screens/InputDiscount";
 import InputItem from "@screens/InputItem";
+import {automatic_sync_background_job } from "../../store/SyncStore/SyncAutomatic";
+
 import translation from "../../translations/translation";
 import LocalizedStrings from "react-native-localization";
 let strings = new LocalizedStrings(translation);
@@ -30,6 +32,14 @@ let strings = new LocalizedStrings(translation);
   "printerStore",
   "syncStore",
   "stateStore",
+    "attendantStore",
+    "receiptStore",
+    "paymentStore",
+    "shiftStore",
+    "shiftReportsStore",
+    "customerStore",
+    "roleStore",
+    "headSyncStore",
 )
 @observer
 export default class ListingContainer extends React.Component {
@@ -510,6 +520,7 @@ export default class ListingContainer extends React.Component {
         type: "danger",
       });
     }
+      automatic_sync_background_job(this.props)
   }
 
   onItemEdit(item) {
