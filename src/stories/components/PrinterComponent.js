5f3355b66c0e798ea1787483720a5e8a13ce67ff
Ivan Ray Altomera <irayspacii@gmail.com>
2019-08-06 10:32:03 +0800

    refactor(PrinterComponent): componentize and memoized variables

--- src/stories/components/PrinterComponent.js
+++ src/stories/components/PrinterComponent.js
@@ -1,49 +1,86 @@
 import * as React from "react";
-import { View, TouchableOpacity, Dimensions } from "react-native";
+import { View, TouchableOpacity, Dimensions, StyleSheet } from "react-native";
 import { Label, Text } from "native-base";
 import { currentLanguage } from "../../translations/CurrentLanguage";
+
 
 import Icon from "react-native-vector-icons/MaterialCommunityIcons";
 import translation from "../.././translations/translation";
 import LocalizedStrings from "react-native-localization";
 let strings = new LocalizedStrings(translation);
-export default class PrinterComponent extends React.Component {
-  render() {
-    strings.setLanguage(currentLanguage().companyLanguage);
 
-    const connectionStatus = this.props.connection ? (
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Icon name="circle" style={{ color: "green" }} />
-        <Text style={{ color: "green", marginLeft: 2 }}>{strings.Online}</Text>
-      </View>
-    ) : (
-      <View style={{ flexDirection: "row", alignItems: "center" }}>
-        <Icon name="circle" style={{ color: "gray" }} />
-        <Text style={{ color: "gray", marginLeft: 2 }}>
-          {this.props.connectionStatus}
-        </Text>
-      </View>
-    );
+const BLUETOOTH_ICON_SIZE = Dimensions.get("window").width * 0.04;
 
-    return (
-      <View style={this.props.style}>
-        <Label>{strings.PrinterStatus}</Label>
-        <View style={{ flexDirection: "row" }}>
-          <TouchableOpacity
-            onPress={this.props.onPrinterPress}
-            style={{ flexDirection: "row", alignItems: "center" }}
-          >
-            <Icon name="printer" size={36} style={{ marginRight: 10 }} />
-            {connectionStatus}
-          </TouchableOpacity>
-          <Icon
-            name="bluetooth-connect"
-            style={{ color: "blue" }}
-            size={Dimensions.get("window").width * 0.04}
-            onPress={() => this.props.connectDevice()}
-          />
-        </View>
+const StatusComponent = (props) => (
+  <View style={styles.rowCenter}>
+    <Icon name="circle" style={props.online ? styles.green : styles.gray} />
+    <Text style={props.online ? [styles.green, styles.statusText] : [styles.gray, styles.statusText]}>
+      {props.children}
+    </Text>
+  </View>
+);
+
+const PrinterComponent = (props) => {
+
+  strings.setLanguage(currentLanguage().companyLanguage);
+
+  const connectionStatus = (
+    <StatusComponent online={props.connection}>
+      {
+        props.connection
+        ? strings.Online
+        : props.connectionStatus
+      }
+    </StatusComponent>
+  );
+
+  return (
+    <View style={props.style}>
+      <Label>{strings.PrinterStatus}</Label>
+      <View style={styles.innerView}>
+        <TouchableOpacity
+          onPress={props.onPrinterPress}
+          style={styles.rowCenter}
+        >
+          <Icon name="printer" size={36} style={styles.icon} />
+          {connectionStatus}
+        </TouchableOpacity>
+        <Icon
+          name="bluetooth-connect"
+          style={styles.blue}
+          size={BLUETOOTH_ICON_SIZE}
+          onPress={props.connectDevice}
+        />
       </View>
-    );
-  }
-}
+    </View>
+  );
+};
+
+
+const styles = StyleSheet.create({
+  rowCenter: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  green: {
+    color: "green",
+  },
+  blue: {
+    color: "blue",
+  },
+  gray: {
+    color: "gray",
+  },
+  statusText: {
+    marginLeft: 2,
+  },
+  innerView: {
+    flexDirection: "row",
+  },
+  icon: {
+    marginRight: 10,
+  },
+});
+
+
+export default PrinterComponent;
