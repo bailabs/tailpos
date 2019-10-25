0586a48c93e771193c1d420ea440e90ad8e8147f
Bai Web and Mobile Lab <irayspacii@gmail.com>
2019-10-25 18:40:31 +0800

    fix: eslint

--- src/services/unifonic.js
+++ src/services/unifonic.js
@@ -1,18 +1,18 @@
-const UNIFONIC_URL = 'http://api.unifonic.com/rest/Messages/Send';
+const UNIFONIC_URL = "http://api.unifonic.com/rest/Messages/Send";
 
 exports.sendText = async function(appsid, recipient, body) {
-    if (body.length > 160) {
-        throw new Error('Message should be 160 characters or less')
-    }
-    
-    const fetchData = {
-        method: "POST",
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-        },
-        body: `AppSid=${appsid}&Recipient=${recipient}&Body=${body}`
-    }
+  if (body.length > 160) {
+    throw new Error("Message should be 160 characters or less");
+  }
 
-    const response = await fetch(UNIFONIC_URL, fetchData);
-    return response.json();
+  const fetchData = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: `AppSid=${appsid}&Recipient=${recipient}&Body=${body}`,
+  };
+
+  const response = await fetch(UNIFONIC_URL, fetchData);
+  return response.json();
 };
